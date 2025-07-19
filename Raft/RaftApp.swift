66f2ce9df262ee61136034243f3226c02a976212
Adrian Gri <adrian.gri03@gmail.com>
2025-07-19 15:58:50 -0700

    Initial Commit

---
+++ Raft/RaftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaftApp.swift
+//  Raft
+//
+//  Created by Adrian Gri on 2025-07-19.
+//
+
+import SwiftUI
+
+@main
+struct RaftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
